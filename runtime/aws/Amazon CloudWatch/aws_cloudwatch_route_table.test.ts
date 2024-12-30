d34d63cf28fd6b63a86230e906c45f59ae9bd93a
Sachin Ghait <ssghait.007@gmail.com>
2024-12-30 12:36:20 +0530

    NFX-712 whitespace

--- runtime/aws/Amazon CloudWatch/aws_cloudwatch_route_table.test.ts
+++ runtime/aws/Amazon CloudWatch/aws_cloudwatch_route_table.test.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//@ts-nocheck
+// @ts-nocheck
 import { CloudWatchClient, DescribeAlarmsForMetricCommand } from "@aws-sdk/client-cloudwatch";
 import {
 	CloudWatchLogsClient,
